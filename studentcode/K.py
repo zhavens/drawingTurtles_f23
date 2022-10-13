da3347ab943a1418cc383819e68f0735addb7ccf
mysticfist <59211580+mysticfist@users.noreply.github.com>
2022-10-13 13:58:48 -0500

    new k
    fixed the k

--- studentcode/K.py
+++ studentcode/K.py
@@ -1,6 +1,6 @@
 import turtle
 import settings as s
-
+#version two of the K 
 
 def draw():
     starting_pos = s.yertle.pos()
@@ -13,27 +13,22 @@ def draw():
 
     s.yertle.right(180)
 
-    s.yertle.forward(s.BOX_HEIGHT * 0.75)
+    s.yertle.forward(s.BOX_HEIGHT/2)
 
     s.yertle.pendown()
 
     s.yertle.right(45)
 
-    s.yertle.forward(s.BOX_WIDTH)
+    s.yertle.forward(s.BOX_WIDTH*1.4)#DID MATH TO GET THE DIAGONAL LENGTH AND THEN GOT THE RATIO OF IT TO WIDTH
 
-    s.yertle.penup()
-
-    s.yertle.forward(-s.BOX_WIDTH)
-
-    s.yertle.pendown()
-
-    s.yertle.right(135)
-
-    s.yertle.forward(s.BOX_HEIGHT * 0.25)
-
-    s.yertle.left(45)
-
-    s.yertle.forward(s.BOX_WIDTH + 10)
+    s.yertle.right(180)#turn it around and repeat basically
+    s.yertle.forward(s.BOX_WIDTH*1.4)
+    s.yertle.left(90)
+    s.yertle.forward(s.BOX_WIDTH*1.4)
+    
+                   
+    
+    
 
     # return turtle to top left of
     s.yertle.penup()
