a257c9c3a2df4e1459e85cc7ae9438d2bcb1fccd
IXIVIVXXV <115684875+IXIVIVXXV@users.noreply.github.com>
2022-10-13 11:08:56 -0500

    Uploading file for creation of the letter E

---
+++ studentcode/E.py
@@ -0,0 +1,27 @@
+import turtle
+import settings as s
+
+
+def draw():
+    starting_pos = s.yertle.pos()
+
+    s.yertle.forward(s.BOX_WIDTH)
+    s.yertle.right(180)
+    s.yertle.forward(s.BOX_WIDTH)
+    s.yertle.left(90)
+    s.yertle.forward(s.BOX_HEIGHT/2)
+    s.yertle.left(90)
+    s.yertle.forward(s.BOX_WIDTH)
+    s.yertle.right(180)
+    s.yertle.forward(s.BOX_WIDTH)
+    s.yertle.left(90)
+    s.yertle.forward(s.BOX_HEIGHT/2)
+    s.yertle.left(90)
+    s.yertle.forward(s.BOX_WIDTH)
+	
+
+    # return turtle to top left of
+    s.yertle.penup()
+    s.yertle.setpos(starting_pos)
+    s.yertle.setheading(0)
+    s.yertle.pendown()
