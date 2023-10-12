99d378efeecfdc3385384c16b4e8f7c20e2ffa40
Janabaydoun <147746496+Janabaydoun@users.noreply.github.com>
2023-10-12 12:38:44 -0500

    Add files via upload

---
+++ I.py
@@ -0,0 +1,27 @@
+#########################
+# janabaydoun + abigail singh-latulipe
+# class activity
+#########################
+
+# import turtle module
+import turtle
+
+roku = turtle.Turtle()      # create turtle name Roku
+
+
+# Pre-conditions: Turtle name roku exists, east orientation, spawns in top
+# left of bounding box
+
+# create definition function for letter 'I'
+def I():
+    roku.forward(25)
+    roku.backward(12.5)
+    roku.right(90)
+    roku.forward(50)
+    roku.right(90)
+    roku.forward(12.5)
+    roku.backward(25)
+    
+# call the function and draws 'I'
+I()
+    
